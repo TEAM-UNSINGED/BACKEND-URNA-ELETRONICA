ece19583eeb2e4bb58b8f4856017ac466e9ea930
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-04-04 23:40:18 -0300

    feat: add voter controller to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 const { Router } = require('express');
 
 const router = Router();
+const voterController = require('../presentation/controllers/voterController');
 
+router.get('/', (_req, res) => res.send({ message: 'api running' }));
 
-
-
-router
-	.get('/', (_req, res) => res.send({ message: 'api running' }))
-
-	
+//VOTER ROUTES
+router.post('/voter',voterController.store);
 
 module.exports = router;
