6a2d8ffeeb4ce8e0c3c1b8d389a678bf7c4c7cef
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-04-04 23:17:25 -0300

    feat: add candidate model

---
+++ src/domain/models/candidato/candidate.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CandidateSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  party: {
+    type: String,
+    required: true,
+  },
+  number:{
+    type: Number,
+    required: true,
+  },
+  photo:{
+    type: String,
+    required: true
+  },
+  nameVice:{
+    type: String,
+    required: true
+  },
+  photoVice:{
+    type: String,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('candidate', CandidateSchema);
